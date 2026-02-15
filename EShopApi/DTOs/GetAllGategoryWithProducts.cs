3a1b3ad6e35ab7241123dac5b548d41f32462cba
karim_ha78 <karim_ha78@STHQ0128-08>
2026-02-15 17:49:08 +0400

    Start

---
+++ EShopApi/DTOs/GetAllGategoryWithProducts.cs
@@ -0,0 +1,11 @@
+﻿using EShopApi.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace EShopApi.DTOs;
+
+public class GetAllGategoryWithProducts
+{
+    [Required]
+    public string Name { get; set; }
+    public List<Product> Products { get; set; } = new List<Product> { };
+}
--- EShopApi/Repositories/CategoryRepository.cs
+++ EShopApi/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using EShopApi.Context;
+using EShopApi.DTOs;
 using EShopApi.Entities;
 
 namespace EShopApi.Repositories;
@@ -8,4 +9,16 @@ public class CategoryRepository : BaseRepository<Category>
     public CategoryRepository(EShopApiDbContext context) : base(context)
     {
     }
-}
+    public List<GetAllGategoryWithProducts> GetAllGategoryWithProducts()
+    {
+        var products = _context.Products.ToList();
+        var categories = _context.Categories.ToList();
+        var result = from c in categories select
+
+            new GetAllGategoryWithProducts
+            {
+                Name = c.Name,
+
+            }
+        return result.ToList();
+    }
